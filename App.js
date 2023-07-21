ff4259b99b725610aaa0d738565c2c1a04a19ea1
Konrad Krzeminski <majunia28@gmail.com>
2023-07-21 13:08:18 +0200

    fix: remove unnecessary code from App

--- App.js
+++ App.js
@@ -1,24 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import MainPage from "./src/components/MainPage";
 
 export default function App() {
-
-  const [status, setStatus] = useState(true);
-
-  const fetchStatus = async () => {
-    const response = await fetch("https://testitp.best.krakow.pl/status", {
-      method: "GET",
-    });
-    const data = await response.json();
-    console.log(data);
-    setStatus(data);
-    console.log(status);
-  };
-
-  useEffect(() => {
-    fetchStatus();
-  }, []);
 
   return (
     <View style={styles.container}>
